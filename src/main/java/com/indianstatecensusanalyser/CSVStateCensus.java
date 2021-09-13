99415d921b9ac06afd9a1306d32708a5a939c274
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-13 22:04:14 +0530

    [Devesh]: Added ability for analyser to load information from csv

--- src/main/java/com/indianstatecensusanalyser/CSVStateCensus.java
+++ src/main/java/com/indianstatecensusanalyser/CSVStateCensus.java
@@ -1,5 +1,23 @@
 package com.indianstatecensusanalyser;
 
 public class CSVStateCensus {
+    public String state;
+    public long population;
+    public long areaInSqKm;
+    public Integer densityPerSqKm;
 
+
+    public CSVStateCensus() {}
+    
+    public CSVStateCensus(String state, long population, long areaInSqKm, Integer densityPerSqKm) {
+        this.state = state;
+        this.population = population;
+        this.areaInSqKm = areaInSqKm;
+        this.densityPerSqKm = densityPerSqKm;
+    }
+    
+    @Override
+    public String toString() {
+    	return state +" " +population+" " +areaInSqKm+" "+densityPerSqKm; 
+    }
 }
--- src/main/java/com/indianstatecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -1,5 +1,39 @@
 package com.indianstatecensusanalyser;
 
-public class StateCensusAnalyser {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import com.opencsv.CSVReader;
 
+public class StateCensusAnalyser {
+	
+	public static final String PathName = "./src/main/resources/IndiaStateCensusData.csv";
+	public static List<CSVStateCensus> stateCensusArray = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		loadingData();
+		System.out.println(stateCensusArray.size());
+	}
+
+	private static void loadingData() {
+		try (
+
+				FileReader readerFile = new FileReader(PathName); 
+				CSVReader reader = new CSVReader(readerFile);
+			){
+				List<String[]> records = reader.readAll();
+				records.stream().forEach(n->{
+				Iterator<String> rotate = Arrays.stream(n).iterator();
+				String state = rotate.next();
+	            String population = rotate.next();
+	            String areaInSqKm = rotate.next();
+	            String densityPerSqKm = rotate.next();
+	            if (!population.equals("Population")) stateCensusArray.add(new CSVStateCensus(state, Long.parseLong(population), Long.parseLong(areaInSqKm), Integer.parseInt(densityPerSqKm)));
+			});
+		}catch(IOException e) {}
+	}
 }
